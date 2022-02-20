0f6a80946c90933a7b0cca1d389c6ae6f802f3f7
donghyeon0725 <ehdgus5015@gmail.com>
2022-02-20 15:46:01 +0900

    test

--- src/main/java/test/Singleton.java
+++ src/main/java/test/Singleton.java
@@ -6,7 +6,6 @@ public class Singleton {
 
     public static void main(String[] args) {
         UserInformation userInformation1 = UserInformation.getInstance();
-
         UserInformation userInformation2 = UserInformation.getInstance();
 
         System.out.println(userInformation1 == userInformation2);
