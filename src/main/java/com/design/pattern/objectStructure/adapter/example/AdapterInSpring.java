2decb074025638d7ce40f615079267402590b681
김동현 <donghyeon@liveklass.com>
2022-02-26 18:12:46 +0900

    adapter example

---
+++ src/main/java/com/design/pattern/objectStructure/adapter/example/AdapterInSpring.java
@@ -0,0 +1,9 @@
+package com.design.pattern.objectStructure.adapter.example;
+
+public class AdapterInSpring {
+
+    public static void main(String[] args) {
+//        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+//        HandlerAdapter adapter = new HandlerAdapter();
+    }
+}
